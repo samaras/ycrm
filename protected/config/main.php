78b444c291b36020e313cb67d81730bbb71491be
samaras <skomfi@gmail.com>
2014-10-27 20:05:22 +0200

    Bootsrap Extension, Employee model

--- protected/config/main.php
+++ protected/config/main.php
@@ -7,6 +7,7 @@
 // CWebApplication properties can be configured here.
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
+	'defaultController' => 'login',
 	'name'=>'YCRM',
 
 	// preloading 'log' component
@@ -20,14 +21,12 @@ return array(
 
 	'modules'=>array(
 		// uncomment the following to enable the Gii tool
-		/*
 		'gii'=>array(
 			'class'=>'system.gii.GiiModule',
-			'password'=>'Enter Your Password Here',
+			'password'=>'jamesspader',
 			// If removed, Gii defaults to localhost only. Edit carefully to taste.
 			'ipFilters'=>array('127.0.0.1','::1'),
 		),
-		*/
 	),
 
 	// application components
@@ -37,7 +36,6 @@ return array(
 			'allowAutoLogin'=>true,
 		),
 		// uncomment the following to enable URLs in path-format
-		/*
 		'urlManager'=>array(
 			'urlFormat'=>'path',
 			'rules'=>array(
@@ -46,13 +44,13 @@ return array(
 				'<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
 			),
 		),
-		*/
-		'db'=>array(
+
+		/*'db'=>array(
 			'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
-		),
+		),*/
 		// uncomment the following to use a MySQL database
 		'db'=>array(
-			'connectionString' => 'mysql:host=localhost;dbname=ycrmdb',
+			'connectionString' => 'pgsql:host=localhost;port=5432;dbname=ycrmdb',
 			'emulatePrepare' => true,
 			'username' => 'ycrm',
 			'password' => 'bostonlegal',
@@ -84,6 +82,6 @@ return array(
 	// using Yii::app()->params['paramName']
 	'params'=>array(
 		// this is used in contact page
-		'adminEmail'=>'webmaster@example.com',
+		'adminEmail'=>'skomfi@gmail.com',
 	),
 );
---
+++ protected/models/Employee.php
@@ -0,0 +1,156 @@
+<?php
+
+/**
+ * This is the model class for table "employee".
+ *
+ * The followings are the available columns in table 'employee':
+ * @property integer $id
+ * @property integer $departmentid
+ * @property string $firstname
+ * @property string $lastname
+ * @property string $email
+ * @property integer $ext
+ * @property boolean $active
+ * @property string $title
+ * @property string $job_title
+ * @property string $address1
+ * @property string $address2
+ * @property string $city
+ * @property string $state_province
+ * @property integer $country
+ * @property string $mobile
+ * @property string $gender
+ * @property string $bank_name
+ * @property string $bank_account_number
+ * @property string $hiredate
+ * @property string $leavedate
+ */
+class Employee extends CActiveRecord
+{
+	/**
+	 * @return string the associated database table name
+	 */
+	public function tableName()
+	{
+		return 'employee';
+	}
+
+	/**
+	 * @return array validation rules for model attributes.
+	 */
+	public function rules()
+	{
+		// NOTE: you should only define rules for those attributes that
+		// will receive user inputs.
+		return array(
+			array('departmentid, firstname, lastname, email, title, job_title, country, gender, hiredate', 'required'),
+			array('departmentid, ext, country', 'numerical', 'integerOnly'=>true),
+			array('firstname', 'length', 'max'=>20),
+			array('lastname, bank_name, bank_account_number', 'length', 'max'=>40),
+			array('email, address1, address2', 'length', 'max'=>60),
+			array('title', 'length', 'max'=>10),
+			array('job_title, city, state_province', 'length', 'max'=>30),
+			array('mobile', 'length', 'max'=>15),
+			array('gender', 'length', 'max'=>6),
+			array('active, leavedate', 'safe'),
+			// The following rule is used by search().
+			// @todo Please remove those attributes that should not be searched.
+			array('id, departmentid, firstname, lastname, email, ext, active, title, job_title, address1, address2, city, state_province, country, mobile, gender, bank_name, bank_account_number, hiredate, leavedate', 'safe', 'on'=>'search'),
+		);
+	}
+
+	/**
+	 * @return array relational rules.
+	 */
+	public function relations()
+	{
+		// NOTE: you may need to adjust the relation name and the related
+		// class name for the relations automatically generated below.
+		return array(
+		);
+	}
+
+	/**
+	 * @return array customized attribute labels (name=>label)
+	 */
+	public function attributeLabels()
+	{
+		return array(
+			'id' => 'ID',
+			'departmentid' => 'Departmentid',
+			'firstname' => 'Firstname',
+			'lastname' => 'Lastname',
+			'email' => 'Email',
+			'ext' => 'Ext',
+			'active' => 'Active',
+			'title' => 'Title',
+			'job_title' => 'Job Title',
+			'address1' => 'Address1',
+			'address2' => 'Address2',
+			'city' => 'City',
+			'state_province' => 'State Province',
+			'country' => 'Country',
+			'mobile' => 'Mobile',
+			'gender' => 'Gender',
+			'bank_name' => 'Bank Name',
+			'bank_account_number' => 'Bank Account Number',
+			'hiredate' => 'Hiredate',
+			'leavedate' => 'Leavedate',
+		);
+	}
+
+	/**
+	 * Retrieves a list of models based on the current search/filter conditions.
+	 *
+	 * Typical usecase:
+	 * - Initialize the model fields with values from filter form.
+	 * - Execute this method to get CActiveDataProvider instance which will filter
+	 * models according to data in model fields.
+	 * - Pass data provider to CGridView, CListView or any similar widget.
+	 *
+	 * @return CActiveDataProvider the data provider that can return the models
+	 * based on the search/filter conditions.
+	 */
+	public function search()
+	{
+		// @todo Please modify the following code to remove attributes that should not be searched.
+
+		$criteria=new CDbCriteria;
+
+		$criteria->compare('id',$this->id);
+		$criteria->compare('departmentid',$this->departmentid);
+		$criteria->compare('firstname',$this->firstname,true);
+		$criteria->compare('lastname',$this->lastname,true);
+		$criteria->compare('email',$this->email,true);
+		$criteria->compare('ext',$this->ext);
+		$criteria->compare('active',$this->active);
+		$criteria->compare('title',$this->title,true);
+		$criteria->compare('job_title',$this->job_title,true);
+		$criteria->compare('address1',$this->address1,true);
+		$criteria->compare('address2',$this->address2,true);
+		$criteria->compare('city',$this->city,true);
+		$criteria->compare('state_province',$this->state_province,true);
+		$criteria->compare('country',$this->country);
+		$criteria->compare('mobile',$this->mobile,true);
+		$criteria->compare('gender',$this->gender,true);
+		$criteria->compare('bank_name',$this->bank_name,true);
+		$criteria->compare('bank_account_number',$this->bank_account_number,true);
+		$criteria->compare('hiredate',$this->hiredate,true);
+		$criteria->compare('leavedate',$this->leavedate,true);
+
+		return new CActiveDataProvider($this, array(
+			'criteria'=>$criteria,
+		));
+	}
+
+	/**
+	 * Returns the static model of the specified AR class.
+	 * Please note that you should have this exact method in all your CActiveRecord descendants!
+	 * @param string $className active record class name.
+	 * @return Employee the static model class
+	 */
+	public static function model($className=__CLASS__)
+	{
+		return parent::model($className);
+	}
+}
