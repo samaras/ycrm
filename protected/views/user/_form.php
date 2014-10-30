3faef93923a89efe358fd4c05a8425ba8c5c8dd3
samaras <skomfi@gmail.com>
2014-10-30 17:20:30 +0200

    User Model2

---
+++ protected/views/user/_form.php
@@ -0,0 +1,64 @@
+<?php
+/* @var $this UserController */
+/* @var $model User */
+/* @var $form CActiveForm */
+?>
+
+<div class="form">
+
+<?php $form=$this->beginWidget('CActiveForm', array(
+	'id'=>'user-form',
+	// Please note: When you enable ajax validation, make sure the corresponding
+	// controller action is handling ajax validation correctly.
+	// There is a call to performAjaxValidation() commented in generated controller code.
+	// See class documentation of CActiveForm for details on this.
+	'enableAjaxValidation'=>false,
+)); ?>
+
+	<p class="note">Fields with <span class="required">*</span> are required.</p>
+
+	<?php echo $form->errorSummary($model); ?>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'username'); ?>
+		<?php echo $form->textField($model,'username',array('size'=>50,'maxlength'=>50)); ?>
+		<?php echo $form->error($model,'username'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'password'); ?>
+		<?php echo $form->passwordField($model,'password',array('size'=>50,'maxlength'=>50)); ?>
+		<?php echo $form->error($model,'password'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'email'); ?>
+		<?php echo $form->textField($model,'email',array('size'=>60,'maxlength'=>355)); ?>
+		<?php echo $form->error($model,'email'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'inactive'); ?>
+		<?php echo $form->checkBox($model,'inactive'); ?>
+		<?php echo $form->error($model,'inactive'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'created_on'); ?>
+		<?php echo $form->textField($model,'created_on'); ?>
+		<?php echo $form->error($model,'created_on'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'last_login'); ?>
+		<?php echo $form->textField($model,'last_login'); ?>
+		<?php echo $form->error($model,'last_login'); ?>
+	</div>
+
+	<div class="row buttons">
+		<?php echo CHtml::submitButton($model->isNewRecord ? 'Create' : 'Save'); ?>
+	</div>
+
+<?php $this->endWidget(); ?>
+
+</div><!-- form -->
---
+++ protected/views/user/_search.php
@@ -0,0 +1,50 @@
+<?php
+/* @var $this UserController */
+/* @var $model User */
+/* @var $form CActiveForm */
+?>
+
+<div class="wide form">
+
+<?php $form=$this->beginWidget('CActiveForm', array(
+	'action'=>Yii::app()->createUrl($this->route),
+	'method'=>'get',
+)); ?>
+
+	<div class="row">
+		<?php echo $form->label($model,'user_id'); ?>
+		<?php echo $form->textField($model,'user_id'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->label($model,'username'); ?>
+		<?php echo $form->textField($model,'username',array('size'=>50,'maxlength'=>50)); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->label($model,'email'); ?>
+		<?php echo $form->textField($model,'email',array('size'=>60,'maxlength'=>355)); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->label($model,'inactive'); ?>
+		<?php echo $form->checkBox($model,'inactive'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->label($model,'created_on'); ?>
+		<?php echo $form->textField($model,'created_on'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->label($model,'last_login'); ?>
+		<?php echo $form->textField($model,'last_login'); ?>
+	</div>
+
+	<div class="row buttons">
+		<?php echo CHtml::submitButton('Search'); ?>
+	</div>
+
+<?php $this->endWidget(); ?>
+
+</div><!-- search-form -->
